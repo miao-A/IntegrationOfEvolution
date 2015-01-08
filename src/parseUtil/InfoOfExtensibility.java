50e10f2631788417899bf4ecdfd89bb0160529da
miao-A <miaoyiang@gmail.com>
2015-01-08 10:43:19 +0800

    Chart show

---
+++ src/parseUtil/InfoOfExtensibility.java
@@ -0,0 +1,19 @@
+package parseUtil;
+
+public class InfoOfExtensibility {
+	private double first;
+	private double second;
+	private double third;
+	
+	public InfoOfExtensibility(double first,double second, double third){
+		this.first=first;
+		this.second = second;
+		this.third = third;
+	}
+	
+	public double getThird(){
+		return third;
+	}
+	
+	
+}
