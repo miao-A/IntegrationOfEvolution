02bf3f3edf4cc09f4a61f2a1ed23ca9f9c2464f4
miao-A <miaoyiang@gmail.com>
2015-03-15 21:20:10 +0800

    stop commit, new project EOST-I will use this

--- src/IntergrationEvaluation/IntergrationMain.java
+++ src/IntergrationEvaluation/IntergrationMain.java
@@ -16,13 +16,20 @@ public class IntergrationMain {
 	
 	public static void main(String[] args) throws IOException {
 		
-		String [] arrayOfProjects = {"D:/ProjectEOfHW/heritrix0.2.0","D:/ProjectEOfHW/heritrix0.4.0",
+	/*	String [] arrayOfProjects = {"D:/ProjectEOfHW/heritrix0.2.0","D:/ProjectEOfHW/heritrix0.4.0",
 				"D:/ProjectEOfHW/heritrix0.6.0","D:/ProjectEOfHW/heritrix0.8.0",
 				"D:/ProjectEOfHW/heritrix1.0.0","D:/ProjectEOfHW/heritrix1.10.0",
 				"D:/ProjectEOfHW/heritrix1.12.0","D:/ProjectEOfHW/heritrix1.14.4",
 				"D:/ProjectEOfHW/heritrix-3.0.0","D:/ProjectEOfHW/heritrix-3.1.0"};
+		*/
+//		String [] arrayOfProjects = {"D:/ProjectEOfHW/eclipseCode/eclipse-3.0"};	
+
+		String [] arrayOfProjects = {"D:/ProjectEOfHW/junit3.4","D:/ProjectEOfHW/junit3.5","D:/ProjectEOfHW/junit3.6",
+				"D:/ProjectEOfHW/junit3.7","D:/ProjectEOfHW/junit3.8","D:/ProjectEOfHW/junit3.9","D:/ProjectEOfHW/junit4.0",
+				"D:/ProjectEOfHW/junit4.1","D:/ProjectEOfHW/junit4.2","D:/ProjectEOfHW/junit4.3","D:/ProjectEOfHW/junit4.4",
+				"D:/ProjectEOfHW/junit4.5","D:/ProjectEOfHW/junit4.6","D:/ProjectEOfHW/junit4.7","D:/ProjectEOfHW/junit4.8",
+				"D:/ProjectEOfHW/junit4.9","D:/ProjectEOfHW/junit4.10","D:/ProjectEOfHW/junit4.11"};
 		
-		//String [] arrayOfProjects = {"D:/ProjectEOfHW/eclipseCode/eclipse-3.0/plugins"};	
 		String pathOfProject;
 
 		
--- src/parseUtil/CreateChartServiceImpl.java
+++ src/parseUtil/CreateChartServiceImpl.java
@@ -53,23 +53,28 @@ public class CreateChartServiceImpl {
 	public CreateChartServiceImpl(String[] rowkeys,String[] columKeys,Vector<InfoOfExtensibility> vec){
 
         String[] rowKeys = {"Àæ×Ó", "ÆÏÌÑ"};  
-        String[] columnKeys = {"D:/ProjectEOfHW/heritrix0.2.0","D:/ProjectEOfHW/heritrix0.4.0",
+       /* String[] columnKeys = {"D:/ProjectEOfHW/heritrix0.2.0","D:/ProjectEOfHW/heritrix0.4.0",
 				"D:/ProjectEOfHW/heritrix0.6.0","D:/ProjectEOfHW/heritrix0.8.0",
 				"D:/ProjectEOfHW/heritrix1.0.0","D:/ProjectEOfHW/heritrix1.10.0",
 				"D:/ProjectEOfHW/heritrix1.12.0","D:/ProjectEOfHW/heritrix1.14.4",
 				"D:/ProjectEOfHW/heritrix-3.0.0","D:/ProjectEOfHW/heritrix-3.1.0"};  
+        */
+        String[] columnKeys = {"D:/ProjectEOfHW/junit3.4","D:/ProjectEOfHW/junit3.5","D:/ProjectEOfHW/junit3.6",
+			"D:/ProjectEOfHW/junit3.7","D:/ProjectEOfHW/junit3.8","D:/ProjectEOfHW/junit3.9","D:/ProjectEOfHW/junit4.0",
+			"D:/ProjectEOfHW/junit4.1","D:/ProjectEOfHW/junit4.2","D:/ProjectEOfHW/junit4.3","D:/ProjectEOfHW/junit4.4",
+			"D:/ProjectEOfHW/junit4.5","D:/ProjectEOfHW/junit4.6","D:/ProjectEOfHW/junit4.7","D:/ProjectEOfHW/junit4.8",
+			"D:/ProjectEOfHW/junit4.9","D:/ProjectEOfHW/junit4.10","D:/ProjectEOfHW/junit4.11"};
         
 		double[][] data = new double[2][vec.size()];
 		for (int i = 0; i < vec.size(); i++) {
 			data[0][i]=vec.get(i).getThird();
+			data[1][i]=vec.get(i).getThird();
 		}
 		
-		for (int i = 0; i < vec.size(); i++) {
-			data[1][i]=8;
-		}
+
 		
         CategoryDataset dataset = getBarData(data, rowKeys, columnKeys);  
-        createTimeXYChar("ÕÛÏßÍ¼", "xÖá", "yÖá", dataset, "lineAndShap.png");  
+        createTimeXYChar("JUnit", "xÖá", "yÖá", dataset, "lineAndShap.png");  
 		
 	}
 	 /** 
